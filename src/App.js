9ceb3cd692c222a3a7fe7ee5a576b8abe122f728
Adrian Pop <adrianpop@Adrians-MBP.fritz.box>
2023-01-23 18:28:28 +0100

    added env in Dockerfile

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          {`Hello ${process.env.REACT_APP_NAME}`}...
+          {`This is the ${process.env.NODE_ENV}`} server...
         </p>
         <a
           className="App-link"
